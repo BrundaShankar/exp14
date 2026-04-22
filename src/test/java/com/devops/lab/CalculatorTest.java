38af3a3b991815a2b4e842ac897f605045c3342e
Brunda <131005645+BrundaShankar@users.noreply.github.com>
2026-04-22 21:52:16 +0530

    Update CalculatorTest.java

--- src/test/java/com/devops/lab/CalculatorTest.java
+++ src/test/java/com/devops/lab/CalculatorTest.java
@@ -1,6 +1,6 @@
-package com.devops.lab;
+package com.bnmit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -13,37 +13,17 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testAddition2() {
-        assertEquals(10, calc.add(5,5));
+    public void testSubtract() {
+        assertEquals(2, calc.subtract(5, 3));
     }
 
     @Test
-    public void testAddition3() {
-        assertEquals(7, calc.add(3,4));
+    public void testMultiply() {
+        assertEquals(6, calc.multiply(2, 3));
     }
 
     @Test
-    public void testNegativeAddition() {
-        assertEquals(-2, calc.add(-1,-1));
-    }
-
-    @Test
-    public void testDivision() {
-        assertEquals(2, calc.divide(10,5));
-    }
-
-    @Test
-    public void testDivisionNegative() {
-        assertEquals(-2, calc.divide(-10,5));
-    }
-
-    @Test
-    public void testDivideByZero() {
-        try {
-            calc.divide(10,0);
-            fail("Expected exception not thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Cannot divide by zero", e.getMessage());
-        }
+    public void testDivide() {
+        assertEquals(2, calc.divide(6, 3));
     }
 }
